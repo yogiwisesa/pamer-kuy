294bf457f063db70405e2de048b076708596fc25
yogiwisesa <yogiwisesa@gmail.com>
2019-03-15 21:58:49 +0700

    first unit test

--- screens/StatusScreen/StatusScreen.js
+++ screens/StatusScreen/StatusScreen.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import { View, Text } from 'react-native';
 
 export class StatusScreen extends React.Component {
-  
+
+  render() {
+    return (
+      <View>
+        <Text>Hello world</Text>
+      </View>
+    );
+  };
 };
--- screens/StatusScreen/__tests__/StatusScreen.spec.js
+++ screens/StatusScreen/__tests__/StatusScreen.spec.js
@@ -1,1 +1,10 @@
-import 
+import React from 'react';
+import { shallow } from 'enzyme';
+import { StatusScreen } from '../StatusScreen';
+import { View } from 'react-native';
+
+test('Should render StatusScreen correctly', () => {
+  const wrapper = shallow(<StatusScreen/>);
+
+  expect(wrapper.find(View)).toHaveLength(1);
+});
