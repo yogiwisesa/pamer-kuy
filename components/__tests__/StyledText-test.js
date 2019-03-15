89388576b8265e517d85f03df3e1db80d45dbf8c
yogiwisesa <yogiwisesa@gmail.com>
2019-03-15 21:51:57 +0700

    enzyme

--- components/__tests__/StyledText-test.js
+++ components/__tests__/StyledText-test.js
@@ -2,9 +2,11 @@ import 'react-native';
 import React from 'react';
 import { MonoText } from '../StyledText';
 import renderer from 'react-test-renderer';
-
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
 it('renders correctly', () => {
-  const tree = renderer.create(<MonoText>Snapshot test!</MonoText>).toJSON();
+  // const tree = renderer.create(<MonoText>Snapshot test!</MonoText>).toJSON();
 
-  expect(tree).toMatchSnapshot();
+  const wrapper = shallow(<MonoText>Snapshot test!</MonoText>);
+  expect(toJson(wrapper)).toMatchSnapshot();
 });
