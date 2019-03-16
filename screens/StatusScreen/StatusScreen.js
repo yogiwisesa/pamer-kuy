90fcc6d43730a3a4a4bbfa36d1935db1fa57a1b6
yogiwisesa <yogiwisesa@gmail.com>
2019-03-16 09:41:43 +0700

    before install native base

--- screens/StatusScreen/StatusScreen.js
+++ screens/StatusScreen/StatusScreen.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, StyleSheet, TextInput, Button, SafeAreaView } from 'react-native';
 
 export class StatusScreen extends React.Component {
-
   render() {
     return (
-      <View>
-        <Text>Hello world</Text>
+      <SafeAreaView style={{flex: 1}}>
+          <View style={{flex: 1}}>
+          <Button title="What are you doing?"/>
+          <Button title="Tell the world now!"/>
+          <TextInput style={{padding: 4, borderWidth: 1}}placeholder="What are you thinking?"/>
       </View>
+      </SafeAreaView>
     );
   };
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 8,
+    flex: 1
+  },
+  spaceVertical: {
+    marginBottom: 10,
+  }
+}); 
+
+export default StatusScreen;
--- screens/StatusScreen/__tests__/StatusScreen.spec.js
+++ screens/StatusScreen/__tests__/StatusScreen.spec.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { StatusScreen } from '../StatusScreen';
 import { View } from 'react-native';
+import { StatusScreen } from '../StatusScreen';
+import { Input, Button } from 'react-native-elements';
 
 test('Should render StatusScreen correctly', () => {
   const wrapper = shallow(<StatusScreen/>);
 
   expect(wrapper.find(View)).toHaveLength(1);
+  expect(wrapper.find(Input)).toHaveLength(1);
+  expect(wrapper.find(Button)).toHaveLength(2);
 });
